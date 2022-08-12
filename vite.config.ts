e2e08bac51396174f0d2f8ce082c8e254813fb45
Gabriel Toledo <gatoledo1@gmail.com>
2022-08-12 13:57:27 -0300

    PWA enable

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,37 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()]
+  plugins: [
+    react(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'To-Do',
+        short_name: 'To-Do',
+        description: 'Lista de tarefas',
+        theme_color: '#0d0d0d',
+        icons: [
+          {
+            src: 'src/assets/todoLogo.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'src/assets/todoLogo.svg',
+            sizes: '360x360',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'src/assets/todoLogo.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ]
 })
